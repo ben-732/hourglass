de15c9d35fa744df023b9d547e609b4b73bf9d15
Ben <22252563+ben-732@users.noreply.github.com>
2023-06-15 20:13:05 +1200

    overview page start

---
+++ comms-page/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="dark:bg-neutral-900 h-screen w-full flex flex-col items-center p-8">
+      <span className="something dark:text-white text-lg font-bold">
+        Comms?
+      </span>
+    </div>
+  );
+}
+
+export default App;
