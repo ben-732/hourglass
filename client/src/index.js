751d153f6e8195cd879e35f57a162fdbb66063bf
Ben <22252563+ben-732@users.noreply.github.com>
2023-06-17 10:55:24 +1200

    dockers?? AAAA

--- client/src/index.js
+++ client/src/index.js
@@ -6,9 +6,11 @@ import App from "./App";
 import axios from "axios";
 
 // Set base URL depending on environment
-if (process.env.NODE_ENV === "development") {
-  axios.defaults.baseURL = "http://localhost:3001";
-}
+// if (process.env.NODE_ENV === "development") {
+// axios.defaults.baseURL = "http://localhost:3001";
+// axios.defaults.baseURL = "http://192.168.1.7:3001";
+
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
--- server/index.js
+++ server/index.js
@@ -15,22 +15,7 @@ const io = InitSocket(server);
 
 StartMidi();
 
-// var oscServer = new Server(53000, "10.0.56.205", () => {
-//   console.log("[OSC] Server is listening: localhost:53000 xx");
-// });
-
-// oscServer.on("message", function (message) {
-//   if (message[0].includes("led")) return;
-
-//   const command = message.shift();
-
-//   console.log(
-//     `[OSC] [${new Date().toLocaleTimeString()}] Message: ${command} : ${message}`
-//   );
-//   // oscServer.close();
-// });
-
-server.listen(3001, () => {
+server.listen(process.env.PORT || 8080, () => {
   const addr = server.address();
   const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
   console.log("[HTTP] Server listening on port " + bind);
